0cafa0205fee7437bce25b437270d7c2e676f52e
Kodiererin <iamujjwal45@gmail.com>
2023-02-17 00:00:46 +0530

    files

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-app.set('view engine','ejs');               //EJS
-
-// Mongoose Instialisation
-const mongoose = require('mongoose');
+app.set('view engine','ejs');
+const mongoose = require('mongoose')  
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/items',{useNewUrlParser : true});  
+  await mongoose.connect('mongodb://127.0.0.1:27017/buyNowitems',{useNewUrlParser : true}); 
     console.log("Database Connected");
 }
-
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/loginBuyNow',{useNewUrlParser : true});  
-      console.log("Database Connected");
-  }
 mongoose.set('strictQuery', false);
 
-const itemsSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        uppercase : true, 
-        required : true,
-    },
-    productDetails : {
-        type : String,
-    },
-    reviews : {
-        type : Array,
-        default : [],
-    },
-    price : {
-        type : Number,
-        minimum : 0,
-        required : true,
-    },
+const itemSchema = new mongoose.Schema({
     sellerName : {
         type : String,
         boolean : true,
@@ -46,28 +22,64 @@ const itemsSchema = new mongoose.Schema({
     sellerUrl : {
         type : String,
         required : true,
-    }
+    },
+    productDetails : {
+        type : String,
+    },
+    reviews : {
+        type : Array,
+        default : [String],
+    },
+    price : {
+        type : Number,
+        minimum : 0,
+        required : true,
+    },
 });
 
-const loginSchema = new mongoose.Schema({
-    userEmail : {
+const item = mongoose.model('item',new mongoose.Schema({
+    name : {
         type : String,
-        required : true,
-        lowercase : true,
     },
-    password : {
-        type : String,
-        required : true,
-    }
+    innerItem : [itemSchema],
+}));
 
-})
+app.get('/',async function(req,res){    
+    const data = new item({
+        name : 'Pen',
+        innerItem :[{
+            productDetails : 'Blue Colour Pen',
+            reviews :['Great Ink' , 'Good Milege'],
+            price : 21.99,
+            sellerName : 'XYZSeller',
+            sellerUrl : 'www.seller.com'
+        },
+        {
+            productDetails : 'Red Colour Pen',
+            reviews :['Great Ink' , 'Good Milege'],
+            price : 25.99,
+            sellerName : 'XYZSeller',
+            sellerUrl : 'www.seller.com'
+        },
+        {
+            productDetails : 'Black Colour Pen',
+            reviews :['Great Ink' , 'Good Milege'],
+            price : 19.99,
+            sellerName : 'XYZSeller',
+            sellerUrl : 'www.seller.com'
+        }]
+    })
 
-const Item = new mongoose.model('Item',itemsSchema);
-const 
+    // data.save(function(err,succ){
+    //     if(!err && succ){
+    //         console.log("Data Has Been Saved Successfully");
+    //     }
+    // });
 
-
-app.get('/',function(req,res){
-    
+    const myData = item.find({name : 'Pen'},function(err,getData){
+        console.log(getData);
+        res.render('buyer',{getData : getData})
+    });
 })
 
 
